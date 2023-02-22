b9060ec9ca6220c70927ff5fac7fc10dea9bc7b6
zczczczczczczczczczczc <111824819+zczczczczczczczczczczc@users.noreply.github.com>
2023-02-22 14:42:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ data = {"city":{"value":city, "color":get_random_color()},
         "temperature":{"value":temperature,"color":get_random_color()},
         "love_days":{"value":get_count(),"color":get_random_color()},
         "birthday_left":{"value":get_birthday(),"color":get_random_color()},
-        "words":{"value":get_words(),"color":get_random_color()}
+        "words":{"value":get_words(),"color":get_random_color()}}
         
 count = 0
 for user_id in user_ids:
